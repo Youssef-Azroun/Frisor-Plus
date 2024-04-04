a08ca26812039acd2324b05d9d27391707454e4d
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2024-04-05 00:38:40 +0200

    Initial Commit

---
+++ Frisor Plus/Frisor_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frisor_PlusApp.swift
+//  Frisor Plus
+//
+//  Created by Youssef Azroun on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Frisor_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Frisor PlusUITests/Frisor_PlusUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Frisor_PlusUITestsLaunchTests.swift
+//  Frisor PlusUITests
+//
+//  Created by Youssef Azroun on 2024-04-04.
+//
+
+import XCTest
+
+final class Frisor_PlusUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
