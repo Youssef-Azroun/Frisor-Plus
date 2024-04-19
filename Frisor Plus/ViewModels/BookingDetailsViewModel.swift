45d90a6002920671d5bdce1bd3e79d4e428bd0d6
Kanyaton Somjit <113116925+Kanyatonsita@users.noreply.github.com>
2024-04-19 11:49:22 +0200

    SettUpViewModelAndModelForBookingsInfo

---
+++ Frisor Plus/ViewModels/BookingDetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  BookingDetailsViewModel.swift
+//  Frisor Plus
+//
+//  Created by Kanyaton Somjit on 2024-04-19.
+//
+
+import Foundation
+
+class BookingDetailsViewModel: ObservableObject {
+    
+}
---
+++ Frisor Plus/ViewModels/InfoBookingsViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  InfoBookingsViewModel.swift
+//  Frisor Plus
+//
+//  Created by Kanyaton Somjit on 2024-04-19.
+//
+
+import Foundation
+class InfoBookingsViewModel: ObservableObject {
+    
+}
