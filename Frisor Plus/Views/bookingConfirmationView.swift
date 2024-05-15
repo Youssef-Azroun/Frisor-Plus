8d11f50257791d5ba4f2d89089bb6dbb271659ce
youssefazroun <yhma22.azroyo@folkuniversitetet.nu>
2024-05-15 11:04:08 +0200

    build issue fix

--- Frisor Plus/Views/bookingConfirmationView.swift
+++ Frisor Plus/Views/bookingConfirmationView.swift
@@ -16,94 +16,101 @@ struct BookingConfirmationView: View {
     
     var body: some View {
         
-        VStack {
-            Image("Logo")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
-            
-            Text("Bokning bekräftelse")
-                .font(.title2)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 5)
-            LottieView()
-                .frame(width: 10, height: 10)
-            Text("Tack för din bokning hos oss Frisör plus.")
-                .foregroundColor(.black)
-                .multilineTextAlignment(.center)
-            
-            Text("📍Viktigt info!")
-                .foregroundColor(.black)
-                .multilineTextAlignment(.center)
-                .font(.headline)
-            
-            HStack{
-                Spacer()
-                Text("Om du har konto hos oss kan du ser din boknings på mitt konto sida om du inte har konto ta gärna en skämdump för att har din bokning sparat i din telefon.")
+        ZStack {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .padding()
+                
+                Text("Bokning bekräftelse")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 5)
+                
+                Text("Tack för din bokning hos oss Frisör plus.")
                     .foregroundColor(.black)
                     .multilineTextAlignment(.center)
-                Spacer()
-            }
-            .padding(.horizontal)
-            .padding(.bottom, 5)
-            
-            Text("Bokningsdetaljer:")
-                .font(.title2)
-                .fontWeight(.bold)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 20)
-            
-            VStack(alignment: .leading, spacing: 10) {
-                if let userDetails = userDetails {
-                    Text("Namn: \(userDetails.firstName) \(userDetails.lastName)")
+                
+                Text("📍Viktigt info!")
+                    .foregroundColor(.black)
+                    .multilineTextAlignment(.center)
                     .font(.headline)
-                    HStack {
-                        Image(systemName: "envelope")
-                        .foregroundColor(.secondary)
-                    Text("Email: \(userDetails.email)")
+                
+                HStack{
+                    Spacer()
+                    Text("Om du har konto hos oss kan du ser din boknings på mitt konto sida om du inte har konto ta gärna en skämdump för att har din bokning sparat i din telefon.")
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 5)
+                
+                Text("Bokningsdetaljer:")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 20)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    if let userDetails = userDetails {
+                        Text("Namn: \(userDetails.firstName) \(userDetails.lastName)")
+                            .font(.headline)
+                        HStack {
+                            Image(systemName: "envelope")
+                                .foregroundColor(.secondary)
+                        Text("Email: \(userDetails.email)")
+                        }
+                        HStack {
+                            Image(systemName: "phone")
+                                .foregroundColor(.secondary)
+                        Text("Mobil: 0\(String(userDetails.phoneNumber))")
+                        }
                     }
                     HStack {
-                        Image(systemName: "phone")
-                        .foregroundColor(.secondary)
-                    Text("Mobil: 0\(String(userDetails.phoneNumber))")
+                        Image(systemName: "scissors")
+                            .foregroundColor(.secondary)
+                    Text("Typ av besök: \(typeOfCut)")
+                    }
+                    HStack {
+                        Image(systemName: "creditcard")
+                            .foregroundColor(.secondary)
+                    Text("Pris: \(price)")
+                    }
+                    HStack {
+                        Image(systemName: "calendar")
+                            .foregroundColor(.secondary)
+                    Text("Datum: \(selectedDate)")
+                    }
+                    HStack {
+                        Image(systemName: "clock")
+                            .foregroundColor(.secondary)
+                    Text("Tid: \(selectedTime)")
                     }
                 }
-                HStack {
-                    Image(systemName: "scissors")
-                    .foregroundColor(.secondary)
-                Text("Typ av besök: \(typeOfCut)")
-                }
-                HStack {
-                    Image(systemName: "creditcard")
-                    .foregroundColor(.secondary)
-                Text("Pris: \(price)")
-                }
-                HStack {
-                    Image(systemName: "calendar")
-                    .foregroundColor(.secondary)
-                Text("Datum: \(selectedDate)")
-                }
-                HStack {
-                    Image(systemName: "clock")
-                    .foregroundColor(.secondary)
-                Text("Tid: \(selectedTime)")
+                .padding()
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+                
+                Spacer()
+                Spacer()
+                NavigationLink(destination: ContentView()) {
+                    Text("Tillbaka till Första sida")
+                        .padding(10)
+                        .background(Color.brown.opacity(5))
+                        .cornerRadius(10.0)
+                        .foregroundColor(.white)
                 }
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10)
-            .shadow(radius: 5)
-            
-            Spacer()
-            Spacer()
-            NavigationLink(destination: ContentView()) {
-                Text("Tillbaka till Första sida")
-                    .padding(10)
-                    .background(Color.brown.opacity(5))
-                    .cornerRadius(10.0)
-                    .foregroundColor(.white)
-            }
+            LottieView()
+                .frame(width: 300, height: 300)
+                .background(Color.clear)
+                .zIndex(1)
+                .allowsHitTesting(false)
         }
         .navigationBarBackButtonHidden(true)
         Spacer()
@@ -116,3 +123,4 @@ struct bookingConfirmation_Previews: PreviewProvider {
         BookingConfirmationView(selectedDate: "2024-05-01", selectedTime: "15:00", price: "300kr", typeOfCut: "Herrklippning", userDetails: User(email: "john.doe@example.com", firstName: "John", lastName: "Doe", phoneNumber: 123456789))
     }
 }
+
